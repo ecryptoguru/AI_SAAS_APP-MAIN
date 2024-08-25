17ced92b0c73e364a61ab4ef616bacd3c197ad3e
ecryptoguru <ecryptoguru@gmail.com>
2024-08-25 17:21:47 +0530

    commit2

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="FusionIM"
+      uploadPreset="fusion_im"
       options={{
         multiple: false,
         resourceType: "image",
